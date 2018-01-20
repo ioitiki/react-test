060f6fb12e9f06f36b00663b2d1bfb8bea2bd326
Andrew Malkin <andymalkin@hotmail.com>
2018-01-19 20:03:34 -0800

    add proptypes to Ticket component

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ticket(){
+function Ticket(props){
   return (
       <div>
-        <h3>3A - Thato & Haley</h3>
-        <p><em>Firebase entries not saving!</em></p>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
         <hr/>
       </div>
-   );
+  );
 }
+
+Ticket.propTypes = {
+  names: React.PropTypes.string.isRequired,
+  location: React.PropTypes.string.isRequired,
+  issue: React.PropTypes.string
+};
 
 export default Ticket;
