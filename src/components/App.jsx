61d819a9435e6f0f1559dbb5ecf18d8b8d97d354
Andrew Malkin <andymalkin@hotmail.com>
2018-01-24 16:16:05 -0800

    add refs to our newTicketForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
+import About from './About';
 
 function App(){
   return (
@@ -12,6 +13,7 @@ function App(){
       <Switch>
         <Route exact path='/' component={TicketList} />
         <Route path='/newticket' component={NewTicketControl} />
+        <Route path='/about' component={About} />
         <Route component={Error404} />
       </Switch>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import helloHippo from '../assets/images/hello-hippo.jpg';
+// import helloHippo from '../assets/images/hello-hippo.jpg';
 
 function Header(){
   return (
     <div>
-      <img src={helloHippo} />
       <h1>Help Queue</h1>
-      <Link to="/">Home</Link> |
-      <Link to="/newticket">Create Ticket</Link> |
-      <Link to="/about">About</Link>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link> | <Link to="/about">About</Link>
     </div>
   );
 }
