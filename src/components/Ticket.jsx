d49f29da21c169b952eadb8531a6bb78f4460c2f
Andrew Malkin <andymalkin@hotmail.com>
2018-01-25 16:20:18 -0800

    add time ticket has been open feature using Moment

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import Moment from 'moment';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
     <div>
       <h3>{props.location} - {props.names}</h3>
+      <h4>{displayTimeOpen(props.timeOpen)} ago</h4>
       <p><em>{props.issue}</em></p>
       <hr />
     </div>
   );
 }
 
+function displayTimeOpen(timeOpen) {
+  return timeOpen.from(new Moment(), true);
+}
+
 Ticket.propTypes = {
-  names: React.PropTypes.string.isRequired,
-  location: React.PropTypes.string.isRequired,
-  issue: React.PropTypes.string
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string,
+  timeOpen: PropTypes.instanceOf(Moment).isRequired
 };
 
 export default Ticket;
